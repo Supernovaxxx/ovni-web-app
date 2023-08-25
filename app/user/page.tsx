330a091ec5fbb556fff63a05a263f917b5a77eea
julianastahelin <julianastahelin@gmail.com>
2023-08-25 18:02:59 -0300

    refactor: replace useUser with code from useUserInfo - get user details in endpoint /me

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,23 +1,19 @@
 'use client'
-import { useUser } from '../auth/useUser'
 import { useRouter } from 'next/navigation'
 import { useToken } from '../auth/useToken'
-import { useUserInfo } from '../auth/useUserInfo'
+import { useUser } from '../auth/useUser'
 
 export default function Page() {
-    const user = useUser()
     const {token, setToken} = useToken()
-    const { user_id } = user
-    const userInfo = useUserInfo()
+    const user = useUser()
     const router = useRouter()
 
-    if (!user) return (router.push('/login'))
+    if (!token) return (router.push('/login'))
 
     return (
             <section className="flex flex-col w-60 p-10 gap-1">
                 <h1 className="font-bold text-lg">User details</h1>
                 <hr />
-                <p>User ID: {user_id}</p>
                 <p>Username: {}</p>
                 <p>Email: {}</p>
             </section>
