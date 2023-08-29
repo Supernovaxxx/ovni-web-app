a396ef91d226c0d98596a09340958900c6715b8a
julianastahelin <julianastahelin@gmail.com>
2023-08-29 00:40:24 -0300

    feat: implement react-query on useUser hookand user page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,18 +4,29 @@ import { useToken } from '../auth/useToken'
 import { useUser } from '../auth/useUser'
 
 export default function Page() {
-    const {token, setToken} = useToken()
-    const user = useUser()
+    const { token } = useToken()
+    const { data, isLoading, isError, failureReason } = useUser()
     const router = useRouter()
+    console.log(data)
+    console.log(failureReason)
 
     if (!token) return (router.push('/login'))
 
     return (
             <section className="flex flex-col w-60 p-10 gap-1">
-                <h1 className="font-bold text-lg">User details</h1>
+                <h1 className="font-bold text-lg">User info</h1>
                 <hr />
-                <p>Username: {}</p>
-                <p>Email: {}</p>
+                <div>
+                    { isError ? 'An error occured' :
+                        isLoading ? 'Loading...' : data &&
+                    <>
+                        <h2><strong>Username:</strong> {data.username}</h2>
+                        <p><strong>Email:</strong> {data.email}</p>
+                        <p><strong>User ID:</strong> {data.pk}</p>
+                        <p><strong>Name:</strong> {data.first_name} {data.last_name}</p>
+                    </>
+                    }
+                </div>
             </section>
     )
 }
