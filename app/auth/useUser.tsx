b4c45a90a855e865a1e0f4d173ee3b143b5b7d45
julianastahelin <julianastahelin@gmail.com>
2023-08-29 00:40:24 -0300

    feat: implement react-query on useUser hookand user page

--- app/auth/useUser.tsx
+++ app/auth/useUser.tsx
@@ -1,27 +1,34 @@
 import { useToken } from './useToken'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
+export interface User {
+    pk: number,
+    username: string,
+    email: string,
+    first_name?: string,
+    last_name?: string,
+}
 
 export function useUser() {
     const { token } = useToken()
+    const [user, setUser] = useState<User>()
 
-    async function getUserInfo() {
-        const response = await axios.get(
-            `${process.env.NEXT_PUBLIC_REST_API_URL}/auth/me`,
-            { headers: { Authorization: `Bearer ${token}` } }
-        )
-        return response
-    }
-
-    const [user, setUser] = useState(() => {
-        if (!token) return ''
-        return getUserInfo()
+    const { data, isLoading, isError, failureReason } = useQuery({
+        queryKey: ['UserInfo'], // this is for caching. not sure how to use it yet
+        queryFn: async () => {
+            const { data } = await axios.get<User>(
+                `${process.env.NEXT_PUBLIC_REST_API_URL}/auth/me`,
+                { headers: { Authorization: `Bearer ${token}` } }
+            )
+            return data as User
+        },
     })
 
     useEffect(() => {
-        if (!token) setUser('')
-        setUser(getUserInfo())
+        setUser(data)
     }, [token])
 
-    return user
+    return { data, isLoading, isError, failureReason }
 }
