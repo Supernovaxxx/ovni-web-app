7f49cf62b78fe26d8c3c913ee47c831f09030d0e
julianastahelin <julianastahelin@gmail.com>
2023-08-30 13:04:17 -0300

    refactor: handle errors in useUser and define types

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,28 +5,37 @@ import { useUser } from '../auth/useUser'
 
 export default function Page() {
     const { token } = useToken()
-    const { data, isLoading, isError, failureReason } = useUser()
     const router = useRouter()
-    console.log(data)
-    console.log(failureReason)
+    const { data, error, isError, isFetching } = useUser(token)
 
-    if (!token) return (router.push('/login'))
-
-    return (
-            <section className="flex flex-col w-60 p-10 gap-1">
-                <h1 className="font-bold text-lg">User info</h1>
-                <hr />
-                <div>
-                    { isError ? 'An error occured' :
-                        isLoading ? 'Loading...' : data &&
-                    <>
-                        <h2><strong>Username:</strong> {data.username}</h2>
-                        <p><strong>Email:</strong> {data.email}</p>
-                        <p><strong>User ID:</strong> {data.pk}</p>
-                        <p><strong>Name:</strong> {data.first_name} {data.last_name}</p>
-                    </>
+    if (isFetching) {
+        return <p className='p-8'>Loading...</p>
+    }
+    if (isError) {
+        if (error.response && error.response.status === 401) {
+            return router.push('/login')
+        } 
+        return (
+            <>
+                <p className='p-8'>Error:
+                    {
+                        error.message && error.response
+                            ? <span>{error.message} - {error.response.statusText}</span>
+                            : <span>{error.message}</span>
                     }
-                </div>
-            </section>
-    )
+                </p>
+            </>
+        )
+    }
+    if (data)
+        return (
+            <div className="p-8 flex flex-col gap-2">
+                <h1 className="text-lg font-bold">User info</h1>
+                <hr />
+                <p><strong>Username:</strong> {data.username}</p>
+                <p><strong>Email:</strong> {data.email}</p>
+                <p><strong>Name:</strong> {data.first_name} {data.last_name}</p>
+                <p><strong>User ID</strong> {data.pk}</p>
+            </div>
+        )
 }
