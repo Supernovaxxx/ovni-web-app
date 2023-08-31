64ac94e8594dcc2f3f61d9c7cba3e5876b0f8427
julianastahelin <julianastahelin@gmail.com>
2023-08-31 14:07:02 -0300

    refactor: set suspense to true in useUser and move loading's fallback to loading.tsx

--- app/(protected-routes)/layout.tsx
+++ app/(protected-routes)/layout.tsx
@@ -1,16 +1,10 @@
 'use client'
-import { useRouter } from 'next/navigation'
 import { useToken } from '../../hooks/useToken'
 import { useUser } from '../../hooks/useUser'
 
 export default function ProtectedLayout({ children, }: { children: React.ReactNode }) {
     const { token } = useToken()
-    const router = useRouter()
-    const { data, isFetching } = useUser(token)
-
-    if (isFetching) {
-        return <p className='p-8'>Loading...</p>
-    }
+    const { data } = useUser(token)
 
     if (data)
         return (
