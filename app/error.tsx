63cd60fa6ccf7ff30d55af78d439bae105de2184
julianastahelin <julianastahelin@gmail.com>
2023-08-31 12:08:56 -0300

    feat: redirect to /login after 3s if status === 401

--- app/error.tsx
+++ app/error.tsx
@@ -10,26 +10,39 @@ export default function Error({
     error: AxiosError
     reset: () => void
 }) {
+    const router = useRouter()
     useEffect(() => {
         console.error(error)
+        if (error.response!.status === 401) {
+            setTimeout(() => { router.push('/login') }, 3000)
+        }
     }, [error])
-    const router = useRouter()
+
     return (
         <div className='p-8'>
-            <h2>Something went wrong!</h2>
-            <p>Error:
-                {
-                    error.message && error.response
-                        ? <span> {error.message} - {error.response.statusText}</span>
-                        : <span> {error.message}</span>
-                }
-            </p>
-            <button className="border-2 p-2 rounded border-slate-800" onClick={() => reset()}>
-                Try again
-            </button>
-            <button className="border-2 p-2 rounded border-slate-800" onClick={() => router.push('/login')}>
-                Log in
-            </button>
+            {
+                error.message && error.response && error.response.status === 401
+                    ? <div className='w-3/6'>
+                        <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/0*aVNnrrTMkCzcY3eJ.jpg" alt="Error 401 - Authorization required" />
+                        <span>Redirecting...</span>
+                        </div>
+                    : <>
+                        <h2>Something went wrong!</h2>
+                        <p>Error:
+                            {
+                                error.message && error.response
+                                    ? <span> {error.message} - {error.response.statusText}</span>
+                                    : <span> {error.message}</span>
+                            }
+                        </p>
+                        <button className="border-2 p-2 rounded border-slate-800" onClick={() => reset()}>
+                            Try again
+                        </button>
+                        <button className="border-2 p-2 rounded border-slate-800" onClick={() => router.push('/login')}>
+                            Log in
+                        </button>
+                    </>
+            }
         </div>
     )
 }
