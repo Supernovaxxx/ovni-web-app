ae69d1393bdd12b048b5ddcc08ca2c630cd86940
julianastahelin <julianastahelin@gmail.com>
2023-09-01 16:01:59 -0300

    refactor: transfer error handling and loading fallback back to layout component

--- app/(protected-routes)/layout.tsx
+++ app/(protected-routes)/layout.tsx
@@ -1,11 +1,22 @@
 'use client'
 import { useToken } from '../../hooks/useToken'
 import { useUser } from '../../hooks/useUser'
+import { useRouter } from "next/navigation"
 
 export default function ProtectedLayout({ children, }: { children: React.ReactNode }) {
     const { token } = useToken()
-    const { data } = useUser(token)
+    const { data, isFetching, error } = useUser(token)
+    const router = useRouter()
 
+    if (isFetching) {
+        return <h1 className='p-8'>Loading...</h1>
+    }
+    if(error) {
+        if (error.response!.status === 401) {
+            setTimeout(() => { router.push('/login') }, 3000)
+        }
+        return <h1 className='p-8'>{error.message} - {error.response!.statusText}</h1>
+    }
     if (data)
         return (
             <div>
