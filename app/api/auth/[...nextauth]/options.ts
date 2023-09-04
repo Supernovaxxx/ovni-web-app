c09ee946c1481c38b51edf85c508a8cfe37d32b7
julianastahelin <julianastahelin@gmail.com>
2023-09-04 17:57:15 -0300

    feat: configure next-auth's route and provider(credentials) and protect routes with middleware

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,25 @@
 import type { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { logIn } from '@/lib/axios-sdk'
 
 export const options: NextAuthOptions = {
     providers: [
-
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: {
+                    label: 'Usuário:',
+                    type: 'text'
+                },
+                password: {
+                    label: 'Senha',
+                    type: 'password'
+                }
+            },
+            async authorize(credentials) {
+                const response = await logIn(credentials!)
+                return (response.status === 200 ? response : null)
+            }
+        })
     ],
 }
