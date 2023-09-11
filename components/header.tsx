1826967e3fe8a48be5958f60cbf7294409e083ba
julianastahelin <julianastahelin@gmail.com>
2023-09-11 19:33:15 -0300

    refactor: define session user's type and update its use on page

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,17 @@
 import { options } from "@/app/api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
 import LogButton from "./log-button"
+import { UserInfo } from '../types/user'
 
 export default async function Header() {
     const session = await getServerSession(options)
+    const user = session?.user as UserInfo | undefined
 
     return (
         <header className='flex justify-end p-4 gap-4 items-center'>
             {session
                 ? <>
-                    <p>{session!.user!.name || session!.user!.email}</p>
+                    <p>{user?.username}</p>
                     <LogButton role='signout'>Log out</LogButton>
                 </>
                 : <LogButton role='signin'>Log in</LogButton>
