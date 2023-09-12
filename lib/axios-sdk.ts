5f8d4b0925d23de5723727ce40088e1f11fed179
julianastahelin <julianastahelin@gmail.com>
2023-09-12 08:59:41 -0300

    feat: add request to protected route '/auth/me'

--- lib/axios-sdk.ts
+++ lib/axios-sdk.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosError } from "axios"
 import { UserCredentials } from "@/types/user"
+import { useQuery } from '@tanstack/react-query'
+import { Token, UserInfo } from '@/types/user'
 
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_REST_API_URL,
@@ -33,4 +35,26 @@ export async function refreshToken(refreshToken: string) {
         return error
     })
     return response
+}
+
+export function getUserInfo(token: Token) {
+    async function getUser() {
+        const { data } = await api<UserInfo>(
+            `/auth/me`,
+            { headers: { Authorization: `Bearer ${token}` } }
+        ).catch(function (error) {
+                if (axios.isAxiosError(error)) {
+                    console.log(error.message)
+                } else {
+                    error = new AxiosError('An unexpected error occurred')
+                }
+                return Promise.reject(error as AxiosError)
+            })
+        return data
+    }
+    return useQuery<UserInfo, AxiosError>({
+        queryKey: ['getUser', token],
+        queryFn: () => getUser(),
+        staleTime: 600000,
+    })
 }
