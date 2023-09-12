8b7f88dee095293ccdaa48e232dd53692d26b00a
julianastahelin <julianastahelin@gmail.com>
2023-09-12 09:03:18 -0300

    refactor: fetch and return user data from protected route with useSession

--- app/(protected-routes)/user/page.tsx
+++ app/(protected-routes)/user/page.tsx
@@ -1,16 +1,21 @@
-import { options } from '../../api/auth/[...nextauth]/options'
-import { getServerSession } from 'next-auth/next'
+'use client'
+import { useSession } from 'next-auth/react'
+import { getUserInfo } from '@/lib/axios-sdk'
+import { Token } from '@/types/user'
 
-export default async function Page() {
-    const session = await getServerSession(options)
+export default function Page() {
+    // To-do: isolate session token retrieving in another file and resolve type
+    const session: any = useSession()
+    const accessToken: Token = session.data?.access_token
+    const meData = getUserInfo(accessToken)
 
+    if (meData && meData.data)
     return (
         <div className="p-8 flex flex-col gap-2">
             <h1 className="text-lg font-bold">User info:</h1>
             <hr />
-            <p>Name: {session!.user!.name}</p>
-            <p>User email: {session!.user!.email}</p>
+            <p>User email: {meData.data.email}</p>
+            <p>Username: {meData.data.username}</p>
         </div>
     )
 }
- 
