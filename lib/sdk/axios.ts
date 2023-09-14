f017f1996946a7fa3e668d4a1116d6dce837361c
julianastahelin <julianastahelin@gmail.com>
2023-09-14 18:37:48 -0300

    refactor: switch getServerSession to getSession

--- lib/sdk/axios.ts
+++ lib/sdk/axios.ts
@@ -1,13 +1,12 @@
-import { options } from "@/app/api/auth/[...nextauth]/options"
 import axios from "axios"
-import { getServerSession } from "next-auth/next"
+import { getSession } from "next-auth/react"
 
 export const api = axios.create({
-    baseURL: process.env.REST_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_REST_API_URL,
 })
 
 api.interceptors.request.use(async (request) => {
-    const session = await getServerSession(options)
+    const session = await getSession()
     if (session) {
         request.headers.Authorization = `Bearer ${session.access}`
     }
